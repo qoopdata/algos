155cd5dcf91414c43bf6aa1c951e3068e7e9c81a
qoopdata <qoopdata@gmail.com>
2023-03-06 16:32:04 +0300

    small refactor and ended examples/custom_sum

--- examples/custom_sum.py
+++ examples/custom_sum.py
@@ -3,4 +3,13 @@ from typing import List
 
 
 def summarize(arr: List[int]) -> int:
-    pass
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        return arr[0] + summarize(arr[1:])
+
+
+
+if __name__ == "__main__":
+    arr = [int(input("Введите число: ")) for i in range(0, int(input("Введите количество чисел массива: ")))]
+    print(f"Результат: {summarize(arr)}")
